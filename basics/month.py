15eb14630d5648e1993bf60bc75744d8d9b8fe82
sripathi <kammari.sripathi@gmail.com>
2021-04-29 09:25:20 +0530

    Create month.py

---
+++ basics/month.py
@@ -0,0 +1,29 @@
+month=input("Enter the month name e.g January")
+
+if month == "Janauary":
+  print("1")
+elif month == "Febraury":
+  print("2")
+elif month == "March":
+  print("3")
+elif month == "April":
+  print("4")
+elif month == "May":
+  print("5")
+elif month == "June":
+  print("6")
+elif month == "July":
+  print("7")
+elif month == "August":
+  print("8")
+elif month == "September":
+  print("9")
+elif month == "October":
+  print("10")
+elif month == "November":
+  print("11")
+elif month == "December":
+  print("12")
+else:
+  print("Please enter valid month name")
+  
